0a8e5fb111446f0bc9ade0a066d8aeeb34bd75e6
Adam Hess <AdamHess@users.noreply.github.com>
2024-02-28 10:23:27 -0500

    Update run_once.sh
    included git jump and vim

--- run_once.sh
+++ run_once.sh
@@ -2,5 +2,10 @@
 source /usr/local/share/nvm/nvm.sh
 nvm install --lts && \
   nvm use --lts && \
-  npm install -g diff-so-fancy
+  npm install -g diff-so-fancy git-jump
+git config alias.sw -e jump
 sudo apt-get update  && sudo apt-get install vim
+
+
+
+
