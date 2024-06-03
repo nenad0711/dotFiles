0093c92e1958ad118eb41f102c24bb11c3e3a8c0
Adam Hess <AdamHess@users.noreply.github.com>
2024-06-03 12:39:58 -0400

    Create .run_once.sh

---
+++ .run_once.sh
@@ -0,0 +1,8 @@
+code --install-extension github.copilot --force
+code --install-extension oderwat.indent-rainbow --force
+code --install-extension hbenl.vscode-test-explorer --force
+code --install-extension samuelcolvin.jinjahtml --force
+code --install-extension leighlondon.eml --force
+code --install-extension joel-harkes.emlviewer --force
+
+
