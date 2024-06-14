ac6224dd3148380106c28925bd66a9f639d2b0d3
Adam Hess <AdamHess@users.noreply.github.com>
2024-06-14 10:42:50 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,8 +2,9 @@
 source /usr/local/share/nvm/nvm.sh
 nvm install --lts && \
   nvm use --lts && \
-  npm install -g diff-so-fancy git-jump
-git config alias.sw -e jump
-sudo apt-get update  && sudo apt-get install -y vim
+  npm install -g diff-so-fancy git-jump &
+git config alias.sw -e jump 
+sudo apt-get update  && sudo apt-get install -y vim & 
+cp . ~/ -r
 
 
