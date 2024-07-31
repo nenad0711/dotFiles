443d1b4ea76c60ad5b650a42239a49bf359013ff
Adam Hess <AdamHess@users.noreply.github.com>
2024-07-31 15:51:52 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
+
+git config alias.sw -e jump 
+cp . ~/ -r
+chmod 755 ~/*.sh
+
 source /usr/local/share/nvm/nvm.sh
 nvm install --lts && \
   nvm use --lts && \
   npm install -g diff-so-fancy git-jump &
-git config alias.sw -e jump 
-sudo apt-get update  && sudo apt-get install -y vim & 
-cp . ~/ -r
-chmod 755 ~/*.sh
-chsh -s /usr/bin/zsh
-chsh -s /usr/bin/zsh vscode
-cp ~/.bash_aliases ~/.zshenv
+  
+sudo apt-get update  && sudo apt-get install -y vim
+
+
